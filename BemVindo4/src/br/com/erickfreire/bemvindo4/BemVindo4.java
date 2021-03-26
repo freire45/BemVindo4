d2ec26c1abec0e4ef6b9fe767ee6d4fb7efc9f19
Erick Freire <erilefr@gmail.com>
2021-03-25 23:00:50 -0300

    BemVindo4
    BemVindo4

---
+++ BemVindo4/src/br/com/erickfreire/bemvindo4/BemVindo4.java
@@ -0,0 +1,14 @@
+package br.com.erickfreire.bemvindo4;
+
+/**
+ * Programa que exibe uma mensagem de boas vindas formatada
+ * @author Erick Freire
+ * @version 1 - Criada em 25-03-2021 as 22:59
+ */
+
+public class BemVindo4 {
+	public static void main(String[] args) {
+		System.out.printf("%s%n%s%n", "Bem vindo", "a Programação em Java!");
+	}
+
+}
